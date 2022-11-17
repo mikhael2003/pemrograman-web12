868629e9686d6bcb8edd8616becec5030ce541f8
JibrilGiovanno <mikhaelsiman2003@gmail.com>
2022-11-17 07:52:52 +0700

    publish branch

---
+++ script.js
@@ -0,0 +1,23 @@
+let kompetisi = document.getElementById("comp");
+let dataKompetisi = document.getElementById("dataComp");
+
+fetch("http://127.0.0.1:5500/tes.json")
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data.standings[0].table)
+
+        data.standings[0].table.forEach(tampil)
+    })
+
+    .catch(error => {
+        console.log(error)
+    });
+
+function tampil(value, index) {
+    dataKompetisi.innerHTML += `<tr>
+    <td>${value.position}</td>
+    <td>${value.team.name}</td>
+    <td>${value.playedGames}</td>
+    <td>${value.points}</td>
+    </tr>`
+}
